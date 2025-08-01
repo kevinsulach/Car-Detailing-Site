dba4360e9e041723439352a0e98254ecf2fae2fa
kevinsulach <kevin.sulach@gmail.com>
2025-08-01 19:53:20 -0400

    Big Update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Simple Add to Cart (redirect only)
-function addToCart(packageName) {
-  localStorage.setItem('selectedPackage', packageName);
+
+function addToCart(name, price) {
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  cart.push({ name, price });
+  localStorage.setItem('cart', JSON.stringify(cart));
   window.location.href = 'checkout.html';
 }
