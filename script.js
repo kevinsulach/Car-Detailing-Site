3713576e598db452c6762c15df49764fd0d65497
kevinsulach <39758062+kevinsulach@users.noreply.github.com>
2025-08-01 20:05:23 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function addToCart(name, price) {
-  const cart = JSON.parse(localStorage.getItem('cart')) || [];
-  cart.push({ name, price });
-  localStorage.setItem('cart', JSON.stringify(cart));
-  window.location.href = 'checkout.html';
-}
